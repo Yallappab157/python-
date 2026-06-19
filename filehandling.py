0df13088e1b9db79a2cd4744285a88cc4a8ead8e
Yallappa <basavarajm9878@gmail.com>
2026-06-19 19:33:24 +0000

    Started file handling read write and append modes...

---
+++ filehandling.py
@@ -0,0 +1,26 @@
+#01 write mode....
+file=open("data.txt","w")
+file.write("Hello python")
+file.close()
+#02 read mode
+file=open("data.txt","r")
+data=file.read()
+print(data)#o/p: Hello python
+file.close()
+#03 checking for other words...
+file=open("data.txt","w")
+file.write("Ram , Hari, Sita")
+file.close()
+file=open("data.txt","r")
+data=file.read()
+print(data)#o/p: Ram, Hari,Sita (older deleted)
+file.close()
+
+# 04 append 'a' adding new element..
+file=open("data.txt","a")
+file.write(" ,Yalla")
+
+#modern code...
+with open("data.txt","r") as file:
+    data=file.read()
+    print(data)#o/p:Ram , Hari, Sita ,Yalla
