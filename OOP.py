dc9371cc42feae6e8b45c0e4079fbf74d8b3f6d2
Yallappa <basavarajm9878@gmail.com>
2026-06-23 18:33:28 +0000

    Started OOP basics

---
+++ OOP.py
@@ -0,0 +1,29 @@
+#01
+class student:
+    pass
+student1=student()
+
+student1.name="Ram"
+student1.marks=90
+print(student1.name)
+print(student1.marks)#o/p:Ram, 90
+
+#02 __init__():....
+class student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+student1=student("Ram",90)
+print(student1.name)
+print(student1.marks)#o/p:Ram, 90
+
+class student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+student1=student("Ram",90)
+student2=student("Hari",80)
+print(student1.name,student1.marks)
+print(student2.name,student2.marks)#o/p:Ram 90, Hari 80
+
+
