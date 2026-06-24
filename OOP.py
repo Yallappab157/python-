e558c71aee4e21134d0c6a73574b57b1fe50be73
Yallappa <basavarajm9878@gmail.com>
2026-06-24 18:29:05 +0000

    Learnt method basic

--- OOP.py
+++ OOP.py
@@ -26,4 +26,23 @@ student2=student("Hari",80)
 print(student1.name,student1.marks)
 print(student2.name,student2.marks)#o/p:Ram 90, Hari 80
 
+# method.......
+class student:
+    def display(self):
+        print("Hello")
+student1=student()
+student1.display()#o/p:Hello
+# printing students data by method..
+class student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
 
+    def display(self):
+        print(self.name)
+        print(self.marks)
+
+student1=student("Ram",90)
+student2=student("Hari",95)
+student1.display()#o/p:Ram, 90
+student2.display()#o/p: Hari, 95
